4ec40839287eb4476296676b8c6c579ad57276bc
mazdik <remappp@gmail.com>
2020-01-20 18:45:43 +0500

    update

--- web-modal/dist/dom-utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function isLeftButton(event: MouseEvent | TouchEvent): boolean;
-export declare function getEvent(event: MouseEvent | TouchEvent): MouseEvent | Touch;
-export declare function maxZIndex(selectors?: string): number;
