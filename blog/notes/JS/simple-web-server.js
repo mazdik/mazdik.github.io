4f32348029038ace3978cec868530ef2ff14164b
mazdik <remappp@gmail.com>
2020-06-12 10:19:31 +0500

    update

--- blog/notes/JS/simple-web-server.js
+++ blog/notes/JS/simple-web-server.js
@@ -17,28 +17,32 @@ collectData = (request, callback) => {
   });
 };
 
-const handler1 = (request, response) => {
+const handler1 = (request, response, query) => {
+  if (Object.keys(query).length) {
+    console.log(query);
+  }
   sendResponse(response, { result: 'Hello World' }, 200);
 };
 
-const handler2 = (request, response) => {
+const handler2 = (request, response, query) => {
   collectData(request, (formattedData) => {
     console.log(formattedData);
     sendResponse(response, { result: 'Success' }, 200);
   });
 };
 
-const routes = {
-  '/': { method: 'GET', handler: handler1 },
-  '/test': { method: 'POST', handler: handler2 },
-};
+const routes = [
+  { url: '/', method: 'GET', handler: handler1 },
+  { url: '/test', method: 'GET', handler: handler1 },
+  { url: '/test', method: 'POST', handler: handler2 }
+];
 
 const server = http.createServer((request, response) => {
-  const parts = url.parse(request.url);
-  const route = routes[parts.pathname];
+  const parts = url.parse(request.url, true);
+  const route = routes.find(x => x.url === parts.pathname && x.method === request.method);
 
-  if (route && route.method === request.method) {
-    route.handler(request, response);
+  if (route) {
+    route.handler(request, response, parts.query);
   } else {
     sendResponse(response, "Not found", 404);
   }
