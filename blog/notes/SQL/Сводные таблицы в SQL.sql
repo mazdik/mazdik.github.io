4d7eaa9ae5ab8f377f6941d8d84cb62abf3c387e
mazdik <remappp@gmail.com>
2020-06-29 22:29:52 +0500

    update

---
+++ blog/notes/SQL/Сводные таблицы в SQL.sql
@@ -0,0 +1,10 @@
+select coalesce(t.product, 'total_sum') as product,
+       sum(case when t.supplier = 'A' then t.volume end) as A,
+       sum(case when t.supplier = 'B' then t.volume end) as B,
+       sum(case when t.supplier = 'C' then t.volume end) as C,
+       sum(t.volume) as total_sum
+from test_supply t
+group by rollup(t.product);
+/*
+группировка строк в колонки, итоги по колонкам, итоги по строкам (с помощью rollup)
+*/
