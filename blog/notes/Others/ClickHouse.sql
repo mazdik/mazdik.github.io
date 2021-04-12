72fe93c61d7b716c144fd4d31984322d5e07ee37
mazdik <remappp@gmail.com>
2021-04-12 11:02:17 +0500

    up

--- blog/notes/Others/ClickHouse.sql
+++ blog/notes/Others/ClickHouse.sql
@@ -9,3 +9,27 @@ SELECT
     count() AS numbers
 FROM events
 GROUP BY ts
+
+/* Sessions */
+SELECT query_id, user, address, elapsed, query   FROM system.processes ORDER BY query_id;
+SELECT * FROM system.metrics WHERE metric LIKE '%Connection';
+
+/* Lock (UPDATE and DELETE) */
+SELECT * FROM system.mutations; 
+
+/* ReplacingMergeTree and PARTITION */
+CREATE TABLE tm_measure
+(
+    well_id UInt64,
+    param_id UInt16,
+    dt DateTime,
+    val Nullable(Float64)
+)
+ENGINE = ReplacingMergeTree
+PARTITION BY toYYYYMM(dt)
+ORDER BY (well_id, param_id, dt)
+SETTINGS index_granularity = 8192;
+
+OPTIMIZE TABLE tm_measure PARTITION '202004';
+
+ALTER TABLE tm_measure DROP PARTITION '202005';
