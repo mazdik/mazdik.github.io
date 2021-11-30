bff494e9db1bf10cb21ec0200236b4f94e4d95d6
mazdik <remappp@gmail.com>
2021-11-30 10:49:34 +0500

    update note

--- blog/notes/Others/ClickHouse.sql
+++ blog/notes/Others/ClickHouse.sql
@@ -33,3 +33,16 @@ SETTINGS index_granularity = 8192;
 OPTIMIZE TABLE tm_measure PARTITION '202004';
 
 ALTER TABLE tm_measure DROP PARTITION '202005';
+
+/* Размер таблиц */
+select concat(database, '.', table)                         as table,
+       formatReadableSize(sum(bytes))                       as size,
+       sum(rows)                                            as rows,
+       max(modification_time)                               as latest_modification,
+       sum(bytes)                                           as bytes_size,
+       any(engine)                                          as engine,
+       formatReadableSize(sum(primary_key_bytes_in_memory)) as primary_keys_size
+from system.parts
+where active
+group by database, table
+order by bytes_size desc;
