4396869e99eaaf2ab23b7c8e6eb72003778e9754
mazdik <remappp@gmail.com>
2024-11-20 10:46:57 +0500

    up notes

--- blog/utils/create-json.js
+++ blog/utils/create-json.js
@@ -2,7 +2,7 @@ const dirTree = require('./directory-tree');
 const fs = require('fs');
 
 const extensions = /\.(md|css|html|js|php|py|cs|sql|txt|jpg|png|sh|bat)$/;
-const tree = dirTree('notes', { extensions });
+const tree = dirTree('notes', { extensions, normalizePath: true });
 
 fs.writeFile('data.json', JSON.stringify(tree), 'utf8', (err) => {
   if (err) return console.log(err);
